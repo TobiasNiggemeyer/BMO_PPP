7ac042050146f4969a53cbeb394e874509f0c8e5
TobiasNiggemeyer <tobiasniggemeyer@googlemail.com>
2019-12-05 15:34:48 +0100

    Interface for tables

--- costfunc.m
+++ costfunc.m
@@ -1,4 +1,32 @@
-function [cost] = costfunc(bee,stock)
+function [cost] = costfunc(beeMatrix,stockMatrix,cocktailMatrix)
+
+    %get size of cocktailMatrix
+    [cocktails_cnt, ingredients_cnt] = size(cocktailMatrix);
+    
+    %vector with the length of the ingredients 
+    stock = zeros(1,ingredients_cnt);
+    
+    %get size of stockMatrix
+    [stock_cnt, amount_cnt] = size(stockMatrix);
+    
+    %write stock amount into respective cells
+    for idx=1:stock_cnt
+        stock_idx = find(strcmpi(cocktailMatrix.Properties.VariableNames,stockMatrix{idx,1}(1,1)));
+        stock(1,stock_idx) = stockMatrix{idx,2}(1,1);
+    end
+    
+    % get size of bee
+    [n, temp] = size(beeMatrix);
+    
+    for idx=1:n
+        table = beeMatrix{idx,1}(1,2:end);
+        bee = [bee; table2array(table)];
+    end
+    
+    %replace NaN with 0
+    bee(isnan(bee)) = 0;
+    
+
 
     % get count of different ingredients
     n_stock = sum(logical(stock));
--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 %read in cocktail Matrix
 cocktailMatrix = readtable('/home/tobias/Development/BMO_PPP/Cocktail_Database/cocktails.csv');
 
+%read in ingredients Matrix
+stockMatrix = readtable('/home/tobias/Development/BMO_PPP/Cocktail_Database/available_ingredients.csv');
+
 %create 5 random bees with 6 recepies
 rand_bees = createRandomBees(cocktailMatrix,5,6);
 
