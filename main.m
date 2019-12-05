a45ea068ae26dadd280898a503c60885a8bb9609
Marian <marianrenz@web.de>
2019-12-05 23:04:57 +0100

    transfer laptop pc

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 %read in cocktail Matrix
-cocktailMatrix = readtable('/home/tobias/Development/BMO_PPP/Cocktail_Database/cocktails.csv');
+cocktailMatrix = readtable('Cocktail_Database/cocktails.csv');
 
 %read in ingredients Matrix
-stockMatrix = readtable('/home/tobias/Development/BMO_PPP/Cocktail_Database/available_ingredients.csv');
+stockMatrix = readtable('Cocktail_Database/available_ingredients.csv');
 
 %create 5 random bees with 6 recepies
 rand_bees = createRandomBees(cocktailMatrix,5,6);
